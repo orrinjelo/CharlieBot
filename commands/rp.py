19e764bf73972fe30c68fd6b482873cfec58d220
Tyler Park <tyler.park@fortemtech.com>
2019-10-17 13:14:35 -0600

    Character creation test

--- commands/rp.py
+++ commands/rp.py
@@ -81,13 +81,13 @@ class Roleplay(commands.Cog):
         self.xp.insert_one(ret)
         return ret
 
-    async def create_character(self, ctx=None, message=None, member=None):
+    async def create_character(self, ctx, name):
         if not message:
             message = ctx.message
-        if not member:
-            member = message.author
+        member = message.author
         player_id = hash(member)
         player_name = str(member)
+        char_name = name
         res = self.characters.find_one(
             {
                 'name': player_name
@@ -100,7 +100,18 @@ class Roleplay(commands.Cog):
         ret = {
             'id': player_id,
             'name': player_name,
+            'wounds': 0,
+            'strain': 0,
+            'soak': 0,
+            'defense': {
+                'ranged': 0,
+                'melee': 0
+            },
             'character': {
+                'name': char_name,
+                'career': '',
+                'specializations': []
+                'species': '',
                 'base_skills': {
                     'INT': 0,
                     'BRA': 0,
@@ -124,16 +135,120 @@ class Roleplay(commands.Cog):
                     'education': 0,
                     'gunnery': 0,
                     'knowledge core worlds': 0,
+                    'knowledge education': 0,
                     'knowledge lore': 0,
                     'knowledge outer rim': 0,
+                    'knowledge underworld': 0,
+                    'knowledge warfare': 0,
+                    'knowledge xenology': 0,
                     'leadership': 0,
                     'lightsaber': 0,
-                    # More to come
-                }
+                    'mechanics': 0,
+                    'medicine': 0,
+                    'melee': 0,
+                    'negotiation': 0,
+                    'perception': 0,
+                    'piloting planetary': 0,
+                    'piloting space': 0,
+                    'ranged heavy': 0,
+                    'ranged light': 0,
+                    'resilience': 0,
+                    'skullduggery': 0,
+                    'stealth': 0,
+                    'streetwise': 0,
+                    'survival': 0,
+                    'vigilance': 0,
+                },
+                'skill_characteristic': {
+                    'INT': [
+                        'astrogation',
+                        'computers',
+                        'knowledge core worlds',
+                        'knowledge education',
+                        'knowledge lore',
+                        'knowledge outer rim',
+                        'knowledge underworld',
+                        'knowledge warfare',
+                        'knowledge xenology',
+                        'cybernetics',
+                        'mechanics',
+                        'medicine'
+                    ],
+                    'BRA': [
+                        'athletics',
+                        'brawl',
+                        'lightsaber',
+                        'melee',
+                        'resilience'
+                    ],
+                    'PRE': [
+                        'charm',
+                        'cool',
+                        'leadership',
+                        'negotiation'
+                    ],
+                    'WIL': [
+                        'coercion',
+                        'discipline',
+                        'vigilance'
+                    ],
+                    'AGI': [
+                        'coordination',
+                        'gunnery',
+                        'piloting planetary',
+                        'piloting space',
+                        'ranged heavy',
+                        'ranged light',
+                        'stealth'
+                    ],
+                    'CUN': [
+                        'deception',
+                        'perception',
+                        'skullduggery',
+                        'streetwise',
+                        'survival'
+                    ]
+                },
+                'duty': {
+                    'type': '',
+                    'points': ''
+                },
+                'critical_injuries': [],
+                'weapons': [],
+                'equipment': [],
+                'talents': [],
+                'credits': [],
+                'max_encumberance': 0,
+                'total_xp': 0,
+                'available_xp': 0,
+                'history_brief': 0,
+                'appearance_brief': 0
             }
         }
         self.characters.insert_one(ret)
         return ret
+
+    async def report_characters_pid(self, ctx, player_id):
+        char = self.characters.find_one(
+            {
+                'player_id': player_id
+            }
+        )
+        # Thanks to IgneelDxD for help on this
+        if str(user.avatar_url)[54:].startswith('a_'):
+            avi = 'https://images.discordapp.net/avatars/' + str(user.avatar_url)[35:-10]
+        else:
+            avi = user.avatar_url        
+        if embed_perms(ctx.message):
+            em = discord.Embed(colour=0x708DD0)
+            em.add_field(name='Player', value=char['player_name'], inline=True)
+            em.add_field(name='Character', value=char['character']['name'], inline=True)
+            em.add_field(name='Characteristics', value=' '.join(['{0}:{1}'.format(s, char['character']['base_skills'][s]) for s in char['character']['base_skills']]), inline=True)
+            em.set_thumbnail(url=avi)
+            await ctx.send(embed=em)
+        else:
+            msg = 'Unimplemented'
+            await ctx.send(msg)        
 
     async def get_player_by_ctx(self, ctx):
         player_id = hash(ctx.message.author)
@@ -165,6 +280,18 @@ class Roleplay(commands.Cog):
         if not res:
             res = await self.create_xp(ctx=ctx, member=member)
         return res
+
+    @commands.command(pass_context=True, aliases=['new_character'])
+    async def newchar(self, ctx, name):
+        await self.create_character(ctx, name)
+        await ctx.send('Character created.')
+
+    @commands.command(pass_context=True, aliases=['character'])
+    async def char(self, ctx):
+        message = ctx.message
+        member = message.author
+        player_id = hash(member)        
+        await self.report_characters_pid(ctx, player_id)
 
     @commands.command(pass_context=True, aliases=['listxpraw'])
     async def xplistraw(self, ctx):
